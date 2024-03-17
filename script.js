b37910f4954bd23cddcf3ddc856f3aa7a5e98a46
Julia Gaugler <greengates4j@gmail.com>
2024-03-17 20:08:17 +0200

    Validate Username function

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-let usernameInput = document.getElementById("user-name");
-let passwordInput = document.getElementById("user-password");
-let submitBtn = document.getElementById("submit-btn");
+var usernameInput = document.getElementById("user-name");
+var passwordInput = document.getElementById("user-password");
+var submitBtn = document.getElementById("submit-btn");
+var userError = document.getElementById("username-error-space");
+var passwordError = document.getElementById("password-error-space");
+var username;
+var password;
+var validUser = false;
+var validPassword = false;
 
-function getUsername() {
-  let username = usernameInput.value;
-  console.log(username);
+function fetchInput() {
+  username = usernameInput.value;
+  password = passwordInput.value;
+  console.log(username, password);
+  validateUsername();
+  console.log(validUser);
 }
 
-function getPassword() {
-  let password = passwordInput.value;
-  console.log(password);
-}
-
-function displayInput() {
-  getUsername();
-  getPassword();
+function validateUsername() {
+  let length = username.length;
+  if (length > 5 && length < 15) {
+    validUser = true;
+    userError.innerHTML = "";
+  } else {
+    userError.innerHTML =
+      "Your username needs to be between 6 and 15 letters long";
+  }
 }
